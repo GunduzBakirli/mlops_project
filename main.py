ce909d53abe09a26a1585f578bc101337d2a91e1
Gunduz Bakirli <Gunduz.bekirli@gmail.com>
2025-12-25 12:18:56 +0400

    fixed bug

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
 app = FastAPI()
-iris=load_iris()
-x=iris.data
-y=iris.target
+
+iris = load_iris()
 model = RandomForestClassifier()
-model.fit(x, y)
-@app.get('/')
+model.fit(iris.data, iris.target)
+
+@app.get("/")
 def home():
-    return {"mesaj":"Real ML modeli ile ishleyen API aktivdir!"}
+    return {"status": "aktiv"}
 
 @app.get("/predict")
 def predict(sepal_l: float, sepal_w: float, petal_l: float, petal_w: float):
-    prediction = model.predict([[sepal_l, sepal_w, petal_l, petal_w]])
-    label = iris.target_names[prediction[0]]
+    data = [[sepal_l, sepal_w, petal_l, petal_w]]
+    prediction = model.predict(data)
+    
+    index = int(prediction[0])
+    label = str(iris.target_names[index])
+    
+    return {
+        "təxmin_edilən_növ": label
+    }
